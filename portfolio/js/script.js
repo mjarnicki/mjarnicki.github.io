98ef2ff41499daa20a3e8f3a5b80ce9d38e3181e
Maciej Jarnicki <mjarnicki@gmail.com>
2018-04-21 23:14:03 +0200

    portfolio z dodanym roll upem

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,14 +1,21 @@
 
 $(function(){
-    var width = window.innerWidth;
-    console.log(width);
-    
     
     $("#menu-responsive").click(function () {
         $("nav ul li").toggleClass("horizontal-menu")
         $("nav").toggleClass("nav-horizontal-invisible");
     });
     
-    
+    $(window).scroll(function(){
+       console.log(window.pageYOffset); 
+        if (window.pageYOffset > 100)
+            {
+                $("#roll-up").css("display", "block");
+            }
+        else
+            {
+                $("#roll-up").css("display", "none");
+            }
+    });
     
 })
