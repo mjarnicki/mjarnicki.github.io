84938bad5e61ecea8c108e2397f7b07d2688561e
Maciej Jarnicki <mjarnicki@gmail.com>
2018-05-04 15:46:10 +0200

    portfolio uzupełniony opis o mnie

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,33 +1,39 @@
  'use strict';
-$(function () {
-   
-    $("#menu-responsive").click(function () {
-        $("nav ul li").toggleClass("horizontal-menu")
-        $("nav").toggleClass("nav-horizontal-invisible");
-    });
+ $(function () {
 
-    $(window).scroll(function () {
-        var position = window.pageYOffset;
+     //hide-show top menu 
+     $("#menu-responsive").click(function () {
+         $("nav ul li").toggleClass("horizontal-menu")
+         $("nav").toggleClass("nav-horizontal-invisible");
+     });
+     //animation of appearing elements
+     $(window).scroll(function () {
+         var position = window.pageYOffset;
 
-        if (position > 100) {
-            $(".roll-up").addClass("roll-up-slide");
-        } else {
-            $(".roll-up").removeClass("roll-up-slide");
-        };
-        
-        if (position > 100) {
-            $(".about-me").addClass("about-me-slide");
-        } else {
-            $(".about-me").removeClass("about-me-slide");
-        }
-        
-        var skillPosition = $(".skill").offset().top - $(".skill").outerHeight();
-        if (position > skillPosition) {
-            $(".skill").addClass("skill-slide");
-        } else {
-            $(".skill").removeClass("skill-slide");
-        }
+         if (position > 100) {
+             $(".roll-up").addClass("roll-up-slide");
+         } else {
+             $(".roll-up").removeClass("roll-up-slide");
+         };
 
-    });
+         if (position > 100) {
+             $(".about-me").addClass("about-me-slide");
+         } else {
+             $(".about-me").removeClass("about-me-slide");
+         }
 
-})
+         var skillPosition = $(".skill").offset().top - $(".skill").outerHeight();
+         if (position > skillPosition) {
+             $(".skill").addClass("skill-slide");
+         } else {
+             $(".skill").removeClass("skill-slide");
+         }
+
+     });
+     // smooth scrool
+     $("[href='#about-me']").click(function () {
+         
+         $('#portfolio').smoothScroll();
+     });
+
+ })
