6b4e066c92cd2cfe4baa4fded458e0586a8ecb8e
Maciej Jarnicki <mjarnicki@gmail.com>
2018-05-07 22:03:47 +0200

    Zadanie domowe AJAX skończone

---
+++ 1-pobierz-dane-programisty/js/script.js
@@ -0,0 +1,24 @@
+    'use strict';
+    $("document").ready(function () {
+
+
+        var ajaxUrl = "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php";
+
+        $("#download").click(function () {
+
+            $.ajax({
+                url: ajaxUrl,
+                dataType: "json",
+                success: function (data) {
+                    showText(data);
+                },
+            })
+        });
+
+        function showText(response) {
+            var daneProgramisty = "Imię: " + response.imie + ",<br> Nazwisko: " + response.nazwisko + ", <br> Zawód: " + response.zawod + ",<br> Firma: " + response.firma;
+            
+            $("#dane-programisty").html(daneProgramisty);
+        }
+
+    });
