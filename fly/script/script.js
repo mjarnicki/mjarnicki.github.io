4fc4d3c45bf57ebc1917221c4c60eea1512bbfd6
Maciej Jarnicki <mjarnicki@gmail.com>
2018-10-10 22:06:16 +0200

    dodanie strzelanie "ręczne"

--- fly/script/script.js
+++ fly/script/script.js
@@ -1,46 +1,73 @@
 $(function () {
     window.addEventListener("keypress", function (event) {
 
-        var direction = event.key;
+        var keyPressed = event.key;
 
         var horizontal = parseInt($("#plane").css("left"));
         var vertical = parseInt($("#plane").css("top"));
-
-        if (direction == "w") {
+        var currentDirection;
+        if (keyPressed == "w") {
             $("#plane").css({
                 "top": (vertical - 20),
                 "transform": "rotate(270deg)"
             });
+            $("#fire").css({ "transform": "rotate(270deg) translate(10px, -10px)" });
         }
 
-        else if (direction == "s") {
+        else if (keyPressed == "s") {
             $("#plane").css({
                 "top": (vertical + 20),
                 "transform": "rotate(90deg)"
             });
+            $("#fire").css({ "transform": "rotate(90deg) translate(10px, 9px)" });
         }
 
-        else if (direction == "a") { 
+        else if (keyPressed == "a") {
             $("#plane").css({
                 "left": (horizontal - 20),
                 "transform": "rotate(180deg)"
-        });
-    }
+            });
+            $("#fire").css({ "transform": "rotate(180deg) translate(20px, -2px)" });
+        }
 
-        else if (direction == "d") { 
+        else if (keyPressed == "d") {
             $("#plane").css({
                 "left": (horizontal + 20),
                 "transform": "rotate(0deg)"
-        });
-    }
-    console.log(horizontal);
-        if (horizontal < 0 || vertical < 0 || horizontal > 680 || vertical > 380){
+            });
+            $("#fire").css({ "transform": "rotate(0deg) translate(0px, 0px)" });
+
+        }
+
+        if (keyPressed == "f") {
+            $("#fire").css({
+                "visibility": "initial",
+                "left": horizontal,
+                "top": vertical
+            });
+        }
+
+        if (keyPressed == "g") {
+            $("#fire").css({
+                "visibility": "hidden",
+                "left": horizontal,
+                "top": vertical
+            });
+        }
+        // if (currentDirection == "down") { }
+        // else if (currentDirection == "up") { }
+        // else if (currentDirection == "left") { }
+        // else if (currentDirection == "right") { }
+
+
+
+        if (horizontal < 0 || vertical < 0 || horizontal > 680 || vertical > 380) {
             $(".speedway").css({
                 "background-image": "url(https://media.giphy.com/media/3o7aD4pR1HbHJFTBF6/giphy.gif)"
-        });
-        $("#plane").css({
-            "display": "none"
-    });
+            });
+            $("#plane").css({
+                "display": "none"
+            });
         }
     })
 })
