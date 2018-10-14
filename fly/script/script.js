ac9404c502acb80b4b69607e71e110dbbdef5c56
Maciej Jarnicki <mjarnicki@gmail.com>
2018-10-14 20:09:05 +0200

    latający helikopter z rakietami

--- fly/script/script.js
+++ fly/script/script.js
@@ -5,61 +5,79 @@ $(function () {
 
         var horizontal = parseInt($("#plane").css("left"));
         var vertical = parseInt($("#plane").css("top"));
-        var currentDirection;
+
+
         if (keyPressed == "w") {
-            $("#plane").css({
-                "top": (vertical - 20),
-                "transform": "rotate(270deg)"
-            });
-            $("#fire").css({ "transform": "rotate(270deg) translate(10px, -10px)" });
+            direction("up")
         }
-
         else if (keyPressed == "s") {
-            $("#plane").css({
-                "top": (vertical + 20),
-                "transform": "rotate(90deg)"
-            });
-            $("#fire").css({ "transform": "rotate(90deg) translate(10px, 9px)" });
+            direction("down")
         }
-
         else if (keyPressed == "a") {
-            $("#plane").css({
-                "left": (horizontal - 20),
-                "transform": "rotate(180deg)"
-            });
-            $("#fire").css({ "transform": "rotate(180deg) translate(20px, -2px)" });
+            direction("left")
         }
-
         else if (keyPressed == "d") {
-            $("#plane").css({
-                "left": (horizontal + 20),
-                "transform": "rotate(0deg)"
-            });
-            $("#fire").css({ "transform": "rotate(0deg) translate(0px, 0px)" });
-
+            direction("right")
+        }
+        else if (keyPressed == "f") {
+            fire(way);
         }
 
-        if (keyPressed == "f") {
+        function direction(currentDirection) {
+            if (currentDirection == "up") {
+                $("#plane").css({
+                    "top": (vertical - 30),
+                    "transform": "rotate(180deg)"
+                });
+            }
+            else if (currentDirection == "down") {
+                $("#plane").css({
+                    "top": (vertical + 30),
+                    "transform": "rotate(0deg)"
+                });
+            }
+            else if (currentDirection == "left") {
+                $("#plane").css({
+                    "left": (horizontal - 30),
+                    "transform": "rotate(90deg)"
+                });
+            }
+            else if (currentDirection == "right") {
+                $("#plane").css({
+                    "left": (horizontal + 30),
+                    "transform": "rotate(270deg)"
+                });
+            }
+            way = currentDirection;
+        }
+
+        function fire(whereToFire) {
             $("#fire").css({
-                "visibility": "initial",
                 "left": horizontal,
-                "top": vertical
-            });
+                "top": vertical,
+                "visibility": "visible"
+            }).attr("src","https://mjarnicki.github.io/fly/img/rockets.png");
+            if (whereToFire == "up") {
+                $("#fire").css({ "transform": "rotate(270deg) translate(0px, -30px)" }).animate(
+                    { top: "0px" }, (vertical * 3), "linear", function(){ $("#fire").attr("src","https://rostfriblog.files.wordpress.com/2016/02/baked-potato-explosion.gif?w=756");}
+                );
+            }
+            else if (whereToFire == "down") {
+                $("#fire").css({ "transform": "rotate(90deg) translate(0px, 30px)" }).animate(
+                    { top: "380px" }, ((400 - vertical) * 3), "linear", function(){ $("#fire").attr("src","https://rostfriblog.files.wordpress.com/2016/02/baked-potato-explosion.gif?w=756");}
+                );
+            }
+            else if (whereToFire == "left") {
+                $("#fire").css({ "transform": "rotate(180deg) translate(30px, 0px)" }).animate(
+                    { left: "0px" }, ((horizontal) * 3), "linear", function(){ $("#fire").attr("src","https://rostfriblog.files.wordpress.com/2016/02/baked-potato-explosion.gif?w=756");}
+                );
+            }
+            else if (whereToFire == "right") {
+                $("#fire").css({ "transform": "rotate(0deg) translate(-30px, 0px)" }).animate(
+                    { left: "680px" }, ((700 - horizontal) * 3), "linear", function(){ $("#fire").attr("src","https://rostfriblog.files.wordpress.com/2016/02/baked-potato-explosion.gif?w=756");}
+                );
+            };
         }
-
-        if (keyPressed == "g") {
-            $("#fire").css({
-                "visibility": "hidden",
-                "left": horizontal,
-                "top": vertical
-            });
-        }
-        // if (currentDirection == "down") { }
-        // else if (currentDirection == "up") { }
-        // else if (currentDirection == "left") { }
-        // else if (currentDirection == "right") { }
-
-
 
         if (horizontal < 0 || vertical < 0 || horizontal > 680 || vertical > 380) {
             $(".speedway").css({
@@ -70,4 +88,5 @@ $(function () {
             });
         }
     })
+    Gifffer();
 })
