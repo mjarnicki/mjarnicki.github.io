434465dac3ca7dcb91c5f89a02378302e871dd4b
Maciej Jarnicki <mjarnicki@gmail.com>
2018-10-16 20:47:19 +0200

    poprawiona animacja wybuchów i dodany licznik amunicji

--- fly/script/script.js
+++ fly/script/script.js
@@ -1,12 +1,17 @@
+
 $(function () {
+    var ammo = 16;
+    $("#ammo").text(ammo);
+
+
+
     window.addEventListener("keypress", function (event) {
 
         var keyPressed = event.key;
-
         var horizontal = parseInt($("#plane").css("left"));
         var vertical = parseInt($("#plane").css("top"));
 
-
+        // buttons
         if (keyPressed == "w") {
             direction("up")
         }
@@ -22,7 +27,7 @@ $(function () {
         else if (keyPressed == "f") {
             fire(way);
         }
-
+        // movement
         function direction(currentDirection) {
             if (currentDirection == "up") {
                 $("#plane").css({
@@ -50,33 +55,44 @@ $(function () {
             }
             way = currentDirection;
         }
-
+        // fire
         function fire(whereToFire) {
-            $("#fire").css({
-                "left": horizontal,
-                "top": vertical,
-                "visibility": "visible"
-            }).attr("src","https://mjarnicki.github.io/fly/img/rockets.png");
-            if (whereToFire == "up") {
-                $("#fire").css({ "transform": "rotate(270deg) translate(0px, -30px)" }).animate(
-                    { top: "0px" }, (vertical * 3), "linear", function(){ $("#fire").attr("src","https://rostfriblog.files.wordpress.com/2016/02/baked-potato-explosion.gif?w=756");}
-                );
+            ammo--
+            if (ammo >= 0) {
+                $("#fire").css({
+                    "left": horizontal,
+                    "top": vertical,
+                    "visibility": "visible"
+                }).attr("src", "https://mjarnicki.github.io/fly/img/rockets.png");
+
+                if (whereToFire == "up") {
+                    $("#fire").css({ "transform": "rotate(270deg) translate(0px, -30px)" }).animate(
+                        { top: "0px" }, (vertical * 3), "linear", function () { explosion()}
+                    );
+                }
+                else if (whereToFire == "down") {
+                    $("#fire").css({ "transform": "rotate(90deg) translate(0px, 30px)" }).animate(
+                        { top: "380px" }, ((400 - vertical) * 3), "linear", function () { explosion()}
+                    );
+                }
+                else if (whereToFire == "left") {
+                    $("#fire").css({ "transform": "rotate(180deg) translate(30px, 0px)" }).animate(
+                        { left: "0px" }, ((horizontal) * 3), "linear", function () { explosion()}
+                    );
+                }
+                else if (whereToFire == "right") {
+                    $("#fire").css({ "transform": "rotate(0deg) translate(-30px, 0px)" }).animate(
+                        { left: "680px" }, ((700 - horizontal) * 3), "linear", function () { explosion()}
+                    );
+                };
+                $("#ammo").text(ammo);
             }
-            else if (whereToFire == "down") {
-                $("#fire").css({ "transform": "rotate(90deg) translate(0px, 30px)" }).animate(
-                    { top: "380px" }, ((400 - vertical) * 3), "linear", function(){ $("#fire").attr("src","https://rostfriblog.files.wordpress.com/2016/02/baked-potato-explosion.gif?w=756");}
-                );
+            function explosion() {
+                $("#fire").attr("src", "https://rostfriblog.files.wordpress.com/2016/02/baked-potato-explosion.gif?w=756");
+                setTimeout(function() {
+                        $('#fire').attr('src',"../img/blank.png")
+                }, 900)
             }
-            else if (whereToFire == "left") {
-                $("#fire").css({ "transform": "rotate(180deg) translate(30px, 0px)" }).animate(
-                    { left: "0px" }, ((horizontal) * 3), "linear", function(){ $("#fire").attr("src","https://rostfriblog.files.wordpress.com/2016/02/baked-potato-explosion.gif?w=756");}
-                );
-            }
-            else if (whereToFire == "right") {
-                $("#fire").css({ "transform": "rotate(0deg) translate(-30px, 0px)" }).animate(
-                    { left: "680px" }, ((700 - horizontal) * 3), "linear", function(){ $("#fire").attr("src","https://rostfriblog.files.wordpress.com/2016/02/baked-potato-explosion.gif?w=756");}
-                );
-            };
         }
 
         if (horizontal < 0 || vertical < 0 || horizontal > 680 || vertical > 380) {
@@ -88,5 +104,5 @@ $(function () {
             });
         }
     })
-    Gifffer();
 })
+
